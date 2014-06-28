54da9ab302875afedf17f5c3349d45ec3fb9ba33
kellerzeus <kellerzeus@163.com>
2014-06-29 00:09:25 +0800

    pitch01
    pitch01

--- js/ssefunction.js
+++ js/ssefunction.js
@@ -3,4 +3,15 @@ $(".extend").click(function(){
     $(this).parent(".player").siblings(".info").slideToggle("fast");
     
   });
+
+$(".subpitch").click(function(){
+    $(".pop").css("display","block");
+    
+  });
+
+$(".pop img").click(function(){
+    $(".pop").css("display","none");
+    
+  });
+
 });
